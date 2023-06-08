bbb5d3474b5b9fbd895da55748d5b011c8e996e3
jeremu2907 <jeremu2907@gmail.com>
2023-06-08 00:13:03 -0500

    added dot product

--- headers/functional.h
+++ headers/functional.h
@@ -13,6 +13,12 @@ namespace functional{
     void DrawCircle(SDL_Renderer * renderer, int32_t centreX, int32_t centreY, int32_t radius);
     void DrawSquare(SDL_Renderer * renderer, int32_t centerX, int32_t centerY, int32_t apothem, float rotation);
     void parseStringToData(std::string s, double *x, double *y, double *vx, double *vy, double *m);
+
+    template <typename T>
+    double dot(T u1, T u2, T v1, T v2)
+    {
+        return v1 * u1 + v2 * u2;
+    }
 }
 
 #endif //PARTICLEPHYSICS_FUNCTIONAL_H
